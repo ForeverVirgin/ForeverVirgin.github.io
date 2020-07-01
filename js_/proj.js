71963fcbcc8e0b270b1cfe134e38ee2c4d09b8ef
ForeverVirgin <67623296+ForeverVirgin@users.noreply.github.com>
2020-07-01 13:06:05 +0800

    Update proj.js

--- js_/proj.js
+++ js_/proj.js
@@ -3,7 +3,7 @@ function init()
 {
 	document.getElementById("gallery").addEventListener("click",sendToGallery);
 	document.getElementById("description").addEventListener("click",sendToDescript);
-	var myMap = new ymaps.Map("map", {
+	var myMap = new ymaps.Map("content", {
             center: [53.27, 108.04],
             zoom: 7
         });
